714f313fd21311109b408c7f1f39b98ef8483810
kernyan <kernyan@users.noreply.github.com>
2022-01-25 17:40:55 -0500

    initial

---
+++ prereq.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+sudo apt-get install gcc libpq-dev -y
+sudo apt-get install python-dev  python-pip -y
+sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
+pip3 install wheel
